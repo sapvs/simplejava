fb86ce07c814242e3b51808b304d535a09adabf9
s3vt <111678562+s3vt@users.noreply.github.com>
2022-10-11 16:10:02 +0530

    git ignore

--- src/main/java/io/s3vt/func/FunctionTest.java
+++ src/main/java/io/s3vt/func/FunctionTest.java
@@ -4,7 +4,7 @@ import java.util.function.BiFunction;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
 
-public class FuncionTest {
+public class FunctionTest {
     public static void main(String[] args) {
 
         Function<String, Integer> function = String::length;
